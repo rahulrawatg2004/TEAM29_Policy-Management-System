4bf1019d96372174fb8d25f4969e770cbf0d1180
Ravi Shankar Kumar <ravishankarphul@gmail.com>
2025-12-04 13:30:16 +0530

    backend

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,49 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const { verifyToken } = require('../config/jwt');
+
+const protect = async (req, res, next) => {
+  let token;
+
+  // Check for token in Authorization header
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      token = req.headers.authorization.split(' ')[1];
+
+      // Verify token
+      const decoded = verifyToken(token);
+
+      // Get user from token (exclude password)
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {
+      console.error(error);
+      return res.status(401).json({ message: 'Not authorized, token failed' });
+    }
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `User role ${req.user.role} is not authorized to access this route`,
+      });
+    }
+    next();
+  };
+};
+
+module.exports = {
+  protect,
+  authorizeRoles,
+};
